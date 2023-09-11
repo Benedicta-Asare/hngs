f5363550cebc62a0b3a7f57bed802a8fa1e5de06
Benedicta-Asare <lilbengilla@gmail.com>
2023-09-11 10:57:40 +0000

    Fixed cors

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
-from datetime import datetime 
+from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/")
 def get_info(slack_name: str, track: str):
+    if not slack_name or not track:
+        return {
+            "status_code": 400,
+            "error": "Please provide slack_name and track"
+            }
+            
     current_day = datetime.now().strftime("%A")
     utc_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     
